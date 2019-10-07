41e23a861615956eadf9f386f4fae6a83de3b601
Yuri47 <yuri.alexs@gmail.com>
2019-10-07 17:59:59 -0300

    'init'

--- js/app.js
+++ js/app.js
@@ -1,36 +1,36 @@
-
-window.scroll({
-
-
-});
-
-
-
-$(".navbar").ready(function() {
-
-
-    jQuery(window).scroll(function() {
-       var windowScrollPosTop = jQuery(window).scrollTop();
-
-       if(windowScrollPosTop >= 150) {
-
-
-         jQuery("nav").addClass("nav-c");
-
-
-       }
-       else{
-         jQuery("nav").removeClass("nav-c");
-
-
-
-       }
-    });
-
-
-
-
-});
-
-
- 
+//
+// window.scroll({
+//
+//
+// });
+//
+//
+//
+// $(".navbar").ready(function() {
+//
+//
+//     jQuery(window).scroll(function() {
+//        var windowScrollPosTop = jQuery(window).scrollTop();
+//
+//        if(windowScrollPosTop >= 150) {
+//
+//
+//          jQuery("nav").addClass("nav-c");
+//
+//
+//        }
+//        else{
+//          jQuery("nav").removeClass("nav-c");
+//
+//
+//
+//        }
+//     });
+//
+//
+//
+//
+// });
+//
+//
+//
